3fc277e0711cab469a80fb35f7eedc5b08eebc3a
Mohammed Sadiq Ali <sadikcp2014@gmail.com>
2024-11-11 22:36:29 +0530

    Add additional fields to billing form for enhanced data collection

--- app.py
+++ app.py
@@ -6,12 +6,43 @@ app = Flask(__name__, template_folder='templates')
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        print(request.form)
         billed_date = request.form.get('billed_date')
         billed_time = request.form.get('billed_time')
         biller_name = request.form.get('billed_by')
         invoice_no = request.form.get('invoice_no')
-        
+        customer_name = request.form.get('customer_name')
+        customer_address = request.form.get('customer_address')
+        business_name = request.form.get('business_name')
+        mode_of_payment = request.form.get('mode_of_payment')
+        service_desc = request.form.getlist('service')
+        service_price = request.form.getlist('price')
+        service_quantity = request.form.getlist('quantity')
+        advance_pay = request.form.get('advance')
+        discount = request.form.get('discount')
+        total_price = request.form.get('total_price')
+        discounted_price = request.form.get('discounted_price')
+        balance_payable = request.form.get('balance_payable')
+
+        context ={
+            'billing_date': billed_date,
+            'billing_time': billed_time,
+            'invoice_no': invoice_no,
+            'biller_name': biller_name,
+            'business_name': business_name,
+            'customer_name': customer_name,
+            'customer_address': customer_address,
+            'mode_of_payment': mode_of_payment,
+            'service_desc': service_desc,
+            'service_price': service_price,
+            'service_quantity': service_quantity,
+            'discount': discount,
+            'total_price': total_price,
+            'advance_pay': advance_pay,
+            'balance_payable': balance_payable,
+            'discounted_price': discounted_price,
+        }
+        print(context)
+
     return render_template('index.html')
 if __name__ == '__main__':
     app.run(debug=True)
